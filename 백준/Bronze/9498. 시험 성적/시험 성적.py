a57640b6aa9fed3e0e7751ec9e1c79fe20d91436
YongDeok_Lim <33304927+ys03055@users.noreply.github.com>
2023-01-18 16:17:04 +0900

    [Bronze V] Title: 시험 성적, Time: 36 ms, Memory: 30616 KB -BaekjoonHub

---
+++ 백준/Bronze/9498. 시험 성적/시험 성적.py
@@ -0,0 +1,11 @@
+s = int(input())
+if s <= 100 and s >=90 :
+    print("A")
+elif s <90 and s >=80:
+    print("B")
+elif s <80 and s >=70:
+    print("C")
+elif s <70 and s >=60:
+    print("D")
+elif s <60 :
+    print("F")
